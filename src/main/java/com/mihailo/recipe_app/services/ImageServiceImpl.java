8924a5945b6fdc302ce08d7f3276815b95024416
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2021-12-21 19:54:32 +0100

    Adding back end for uploading and reading images.

--- src/main/java/com/mihailo/recipe_app/services/ImageServiceImpl.java
+++ src/main/java/com/mihailo/recipe_app/services/ImageServiceImpl.java
@@ -1,14 +1,45 @@
 package com.mihailo.recipe_app.services;
 
+import com.mihailo.recipe_app.model.Recipe;
+import com.mihailo.recipe_app.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl( RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Image uploaded.");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
     }
 }
